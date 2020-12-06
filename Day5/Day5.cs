51caf4f8623ee1cb5ff1c0e85464f50ef2eb5972
JosephEid <josephceid@gmail.com>
2020-12-06 21:53:05 +0000

    Day5 Part2

--- Day5/Day5.cs
+++ Day5/Day5.cs
@@ -17,7 +17,7 @@ namespace Day5
                 } 
                 else if (args[0] == "2")
                 {
-                    // Part2();
+                    Part2();
                 } 
                 else 
                 {
@@ -62,6 +62,44 @@ namespace Day5
                 if (seatId > maxId) maxId = seatId;
             }
             Console.WriteLine(maxId);
+        }
+
+        static void Part2()
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            List<int> seatIds = new List<int>();
+            List<int> initRowRange = new List<int>();
+            for (int i = 0; i <= 127; i++) initRowRange.Add(i);
+            List<int> initColRange = new List<int>();
+            for (int i = 0; i <= 7; i++) initColRange.Add(i);
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                string res = input[i];
+                string row = res.Substring(0, 7);
+                string col = res.Substring(7);
+                List<int> rowRange = initRowRange;
+                List<int> colRange = initColRange;
+                for (int j = 0; j < row.Length; j++)
+                {
+                    if (row[j] == 'B') rowRange = rowRange.Skip(rowRange.Count / 2).ToList();
+                    else if (row[j] == 'F') rowRange = rowRange.Take(rowRange.Count / 2).ToList();
+                    else Console.WriteLine("Unrecognized char");
+                }
+                for (int k = 0; k < col.Length; k++)
+                {
+                    if (col[k] == 'R') colRange = colRange.Skip(colRange.Count / 2).ToList();
+                    else if (col[k] == 'L') colRange = colRange.Take(colRange.Count / 2).ToList();
+                    else Console.WriteLine("Unrecognized char");
+                }
+                int seatId = rowRange[0]*8 + colRange[0];
+                seatIds.Add(seatId);
+            }
+            seatIds.Sort();
+            for (int i = seatIds.First(); i <= seatIds.Last(); i++)
+            {
+                if (!seatIds.Exists(x => x == i)) Console.WriteLine(i);
+            } 
         }
     }
 }
