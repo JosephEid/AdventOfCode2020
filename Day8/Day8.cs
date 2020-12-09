f3f1309ee08fd6ac6adfd7e6d58c42bc48348b17
JosephEid <josephceid@gmail.com>
2020-12-09 22:44:11 +0000

    Day8 Part2

--- Day8/Day8.cs
+++ Day8/Day8.cs
@@ -16,7 +16,7 @@ namespace Day8
                 } 
                 else if (args[0] == "2")
                 {
-                    // Part2();
+                    Part2();
                 } 
                 else 
                 {
@@ -54,6 +54,56 @@ namespace Day8
                 }
             }
             End: Console.WriteLine(acc);
+        }
+
+        static void Part2()
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            List<int> jmpNop = new List<int>();
+            for (int h = 0; h < input.Length; h++) {
+                string[] split = input[h].Split(" ");
+                if (split[0] == "nop" || split[0] == "jmp") jmpNop.Add(h);
+            }
+            foreach (int loc in jmpNop)
+            {
+                int acc = 0;
+                int loopCount = 0;
+                List<int> visited = new List<int>();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (i == input.Length - 1)
+                    {
+                        Console.WriteLine(acc);
+                        goto Found;
+                    } 
+                    if (visited.Contains(i)) break;
+                    if (loopCount > input.Length -1) break;
+                    visited.Add(i);
+                    
+                    string[] split = input[i].Split(" ");
+                    string action = split[0];
+                    if (i == loc)
+                    {
+                        action = action =="jmp" ? "nop" : "jmp";
+                    }
+                    string sign = split[1].Substring(0, 1);
+                    string numS = split[1].Substring(1);
+                    int num = Int32.Parse(numS);
+                    switch (action)
+                    {
+                        case "nop": 
+                            continue;
+                        case "acc":
+                            acc = sign == "+" ? acc + num : acc - num;
+                            break;
+                        case "jmp":
+                            i = sign == "+" ? i + num - 1 : i - num - 1;
+                            break;
+                    }
+                }
+            }
+            Found: Console.WriteLine("Result found ^");
         }
     }
 }
