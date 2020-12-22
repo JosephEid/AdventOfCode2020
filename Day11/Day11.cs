5d5b62a06d879786d628431ed2e85251eee913f6
JosephEid <josephceid@gmail.com>
2020-12-22 20:30:04 +0000

    Day11 Part1

---
+++ Day11/Day11.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day11
+{
+    class Day11
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0] == "1")
+                {
+                    Part1();
+                } 
+                else if (args[0] == "2")
+                {
+                    // Part2();
+                } 
+                else 
+                {
+                    Console.WriteLine($"Please specify which part of the exercise you would like to run e.g dotnet run {"'1'"}");
+                }
+            } 
+            else {
+                Console.WriteLine($"Please specify which part of the exercise you would like to run e.g dotnet run {"'1'"}");
+            }
+        }
+
+        static void Part1()
+        {
+            List<string> input = File.ReadAllLines("input.txt").ToList();
+            List<List<char>> state = new List<List<char>>();
+            List<List<char>> newState = new List<List<char>>();
+
+            for (int i = 0; i < input.Count(); i++)
+            {
+                state.Add(new List<char>());
+                newState.Add(new List<char>());
+                input[i].ToList().ForEach(x => state[i].Add(x));
+                input[i].ToList().ForEach(x => newState[i].Add(x));
+            }
+
+            int width = state[0].Count();
+            int height = state.Count();
+            bool same = false;
+
+            while (!same)
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        char space = state[i][j];
+                        char? n, s, w, e, ne, nw, se, sw;
+                        n = s = w = e = ne = nw = se = sw = null;
+                        
+                        if (i+1 < height) n = state[i+1][j];
+                        if (i-1 >= 0) s = state[i-1][j];
+                        if (j+1 < width) e = state[i][j+1];
+                        if (j-1 >= 0) w = state[i][j-1];
+                        if (i+1 < height && j+1 < width) ne = state[i+1][j+1];
+                        if (i-1 >= 0 && j+1 < width) se = state[i-1][j+1];
+                        if (i+1 < height && j-1 >= 0) nw = state[i+1][j-1];
+                        if (j-1 >= 0 && i-1 >= 0) sw = state[i-1][j-1];
+
+                        List<char?> adj = new List<char?>()
+                        {
+                            n, s, e, w, ne, nw, se, sw
+                        };
+                        
+                        if (space == 'L' && adj.All(x => x!='#'))
+                            newState[i][j] = '#';
+
+                        else if (space == '#' && adj.Where(x => x == '#').Count() >= 4)
+                            newState[i][j] = 'L';
+                    }
+                }
+                bool changeFound = false;
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        if (state[i][j] != newState[i][j]) changeFound = true;
+                        state[i][j] = newState[i][j];
+                    }
+                }
+
+                int occ = 0;
+                foreach (List<char> row in state) occ += row.Where(x => x.Equals('#')).Count();
+                Console.WriteLine(occ);
+                if (!changeFound) same = true;
+            }
+        }
+    }
+}
