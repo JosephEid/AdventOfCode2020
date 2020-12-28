25e8fbd0c2a2792e487b0f1cfe0334d7cd474918
JosephEid <josephceid@gmail.com>
2020-12-28 21:04:48 +0000

    Day13 Part2

--- Day13/Day13.cs
+++ Day13/Day13.cs
@@ -17,7 +17,7 @@ namespace Day13
                 } 
                 else if (args[0] == "2")
                 {
-                    // Part2();
+                    Part2();
                 } 
                 else 
                 {
@@ -34,7 +34,6 @@ namespace Day13
             List<string> input = File.ReadAllLines("input.txt").ToList();
             int timestamp = Int32.Parse(input[0]);
             List<int> busIds = input[1].Split(",").Where(x => x != "x").Select(x => Int32.Parse(x)).ToList();
-            double min = Double.PositiveInfinity;
             busIds.ForEach(delegate(int id)
             {
                 bool exceeded = false;
@@ -50,6 +49,44 @@ namespace Day13
                     multiple ++;
                 }
             });
+        }
+
+        static void Part2()
+        {
+            List<string> input = File.ReadAllLines("input.txt").ToList();
+            List<string> busIds = input[1].Split(",").ToList();
+            int max = 0;
+            List<Int64> indexes = busIds.Where(x => x != "x").Select(x => (Int64)busIds.IndexOf(x)).ToList();
+            indexes.ForEach(x => Console.WriteLine(x));
+            Int64 first = Int64.Parse(busIds.First());
+            Int64 start = 0;
+            for (Int64 i = 100000000000000; i < Single.PositiveInfinity; i++)
+            {
+                if (i % first == 0) 
+                {
+                    start = i;
+                    Console.WriteLine($"Start found: {start}");
+                    goto Start;
+                }
+            }
+            Start: for (Int64 t = start; t < Single.PositiveInfinity; t += first)
+            {
+                if (t%10000000 == 0) Console.WriteLine(t);
+                int count = 0;
+                for (int j = 1; j < indexes.Count(); j++)
+                {
+                    Int64 multiple = Int64.Parse(busIds[(int)indexes[j]]);
+                    if ((t + indexes[j]) % multiple == 0) count++;
+                    else break;
+                }
+                if (count == indexes.Count()-1) 
+                {
+                    Console.WriteLine($"Match found: {t} {max}");
+                    goto End;
+                }
+            }
+
+            End: Console.WriteLine("");
         }
     }
 }
